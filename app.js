b85e886f7ff953968b753819c4e4d71916eebf85
chardi26 <chardi26@uncc.edu>
2023-09-20 17:24:22 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 8084;
+const host = 'localhost';
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    
+    let path = './';
+
+    if(req.url === './'){
+        path = path + 'index.html';
+    }else if (req.url === '/contact'){
+        path = path + 'contact.html';
+    }else if (req.url === '/about'){
+        path = path + 'about.html';
+    }else {
+        res.statusCode = 404;
+        path = path + '404.html';
+    }
+
+    fs.readFile(path, (err,data) =>{
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+});
